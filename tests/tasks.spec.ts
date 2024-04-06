9849195fec89c5995c4b9fa9da4e4a7580a66f48
HigorVenancioQA <higor.venancio.qa@gmail.com>
2024-04-06 11:31:51 -0300

    Inicio projeto mark

---
+++ tests/tasks.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test'
+import { TaskModel } from './fixtures/task.model'
+
+test('Deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
+
+    const task: TaskModel = {
+        name: 'Ler um livro TypeScript',
+        is_done: false
+    }
+
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
+
+    await page.goto('http://localhost:3000')
+
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill(task.name)
+
+    await page.click('css=button >> text=Create')
+
+    const target = page.locator(`css=.task-item p >> text=${task.name}`)
+    await expect(target).toBeVisible
+
+})
+
+test('Não deve permitir tarefa duplicada', async ({ page, request }) => {
+    const task: TaskModel = {
+        name: 'Comprar Ketchup',
+        is_done: false
+    }
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
+
+    const newTask = await request.post('http://localhost:3333/tasks', { data: task })
+    expect(newTask.ok()).toBeTruthy()
+
+    await page.goto('http://localhost:3000')
+
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill(task.name)
+
+    await page.click('css=button >> text=Create')
+
+    const target = page.locator('.swal2-html-container')
+    await expect(target).toHaveText('Task already exists!')
+})
